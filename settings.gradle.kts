3c5882a3e424a2d52381ac0e76b3cc0141ea261c
k_shimoju <k_shimoju@yumemi.co.jp>
2023-07-05 15:32:11 +0900

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+rootProject.name = "Sample202307"
+include(":composeApp")
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
