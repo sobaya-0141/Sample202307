21a1e2796c5d73d20e34abd5bead8ba511587754
k_shimoju <k_shimoju@yumemi.co.jp>
2023-07-07 10:25:39 +0900

    typo修正

--- repository/src/commonMain/kotlin/sobaya/app/repository/Sample.kt
+++ repository/src/commonMain/kotlin/sobaya/app/repository/Sample.kt
@@ -3,7 +3,7 @@ package sobaya.app.repository
 import kotlinx.coroutines.delay
 
 class Sample {
-    suspend fun Sample(): String {
+    suspend fun sample(): String {
         delay(500)
         return "SAMPLE"
     }
